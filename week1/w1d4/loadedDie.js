fd5bb1c9e494cd542c4bc1c446d6bb3396bf4a29
MattEmond <memond36@gmail.com>
2017-11-02 21:40:03 +0000

    Getting somewhere...i think

--- week1/w1d4/loadedDie.js
+++ week1/w1d4/loadedDie.js
@@ -1,10 +1,10 @@
 function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  /* your code here */
+  console.log(list[0]);
 
   return function() {
-    /* your code here */
-  }
+    return list - 1;
+  };
 }
 
 var rollLoadedDie = makeLoadedDie();
