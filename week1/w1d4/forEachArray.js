0076921af02c6afc959198257bd4fae057cfdb2d
MattEmond <memond36@gmail.com>
2017-11-02 19:05:34 +0000

    Re-added

---
+++ week1/w1d4/forEachArray.js
@@ -0,0 +1,20 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  arr.forEach(function(arr, index) {
+    if (arr === 'Waldo') {
+      found(index)
+    }
+  })
+
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found();   // execute callback
+  //   }
+  // }
+};
+
+function actionWhenFound(index) {
+  console.log("Found him at: ", index);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
