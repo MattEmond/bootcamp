c5bb546b2ae1ccbc5032936f84c4fec49a79f448
MattEmond <memond36@gmail.com>
2017-11-02 22:31:42 +0000

    changed current

--- week1/w1d4/loadedDie.js
+++ week1/w1d4/loadedDie.js
@@ -1,10 +1,12 @@
 function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  console.log(list[0]);
+  var current = -1;
 
   return function() {
-    return list - 1;
+    current ++;
+    return list[current];
   };
+
 }
 
 var rollLoadedDie = makeLoadedDie();
