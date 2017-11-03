2e00dc3ca57d608457831efe8a752c8dc2914f80
MattEmond <memond36@gmail.com>
2017-11-03 01:56:31 +0000

    Updated version of code that gives desired output

--- week1/w1d4/loggingWrapper.js
+++ week1/w1d4/loggingWrapper.js
@@ -1,10 +1,12 @@
 var wrapLog = function (callback, name) {
   return function() {
+    var slicedArgs = Array.prototype.slice.call(arguments, 0);
+    console.log(name + "(" + slicedArgs.toString() + ")", "=>", callback.apply(null, arguments));
+
     // console.log(callback)
-    // area.apply(null, arguments)
     // console.log(name)
     // console.log(arguments)
-    console.log(name, arguments, "=>", callback.apply(null, arguments))//(arguments[0],arguments[1],arguments[2]))
+    // console.log(name, arguments, "=>")
   };
 
 };
@@ -24,3 +26,13 @@ var logVolume = wrapLog(volume, "volume");
 
 logVolume(5, 3, 2); // volume(5, 3, 2) => 30
 logVolume(3, 2, 4); // volume(3, 2, 4) => 24
+
+/*
+
+Arguments is an array like object corresponding to the arguments passed
+in the function.  In this code, it is allowing me to apply all arguments (x,y and then x,y,z)
+into my console log.
+
+The apply is letting me apply both functions for my callbacks (area and volume)
+
+*/
