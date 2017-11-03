3bbd2f4ebcec72aabb36d6573b411c1b04ee7eed
MattEmond <memond36@gmail.com>
2017-11-03 12:56:35 +0000

    Description Notes

--- week1/w1d4/maps.js
+++ week1/w1d4/maps.js
@@ -1,12 +1,12 @@
 var words = ["ground", "control", "to", "major", "tom"];
 
 
-var map = function(arr, callback) {
+var map = function(arr, callback) { //On this line, the two arguments passed into the function refer to the "Words" array (arr).  "Callback" refers to the three variables (length, upperCase, and reverse).
   var result = [];
   arr.forEach(function(item) { //the function on this line refers to the fuction on line 7
-    result.push(callback(item)); //this essentially means that for each item in the array, invoke a callback function (map), and push all of those results into my empty array.
+    result.push(callback(item)); //this essentially means that for each item in the array, invoke a callback function (map), and push all of those results into my empty array (result).
   });
-  return result;
+  return result; //This returns my new array filled with all items that have been passed through it.  In this case, tbe three variables (length, upperCase, and reverse)
 };
 
 
