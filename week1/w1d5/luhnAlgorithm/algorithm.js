638f9186703bb87a0c3f44d9588aea1f78cb314c
MattEmond <memond36@gmail.com>
2017-11-03 18:31:49 +0000

    added request

--- week1/w1d5/luhnAlgorithm/algorithm.js
+++ week1/w1d5/luhnAlgorithm/algorithm.js
@@ -1,18 +1,18 @@
-var check = function(number) {
+ var check = function(string) {
 
-}
-
-
-
-check(7992739871)
+ }
 
 
 module.exports = {
-  check: check
+  check check
 }
+
+
+
+
 /*
 
-From the rightmost digit, which is the check digit, and moving left, double the value of every second digit. If the result of this doubling operation is greater than 9 (e.g., 8 × 2 = 16), then add the digits of the product (e.g., 16: 1 + 6 = 7, 18: 1 + 8 = 9) or, alternatively, the same result can be found by subtracting 9 from the product (e.g., 16: 16 - 9 = 7, 18: 18 - 9 = 9).
+From the rightmost digit, which is the check digit, and moving left, double the value of every second digit. If the result of this doubling operation is greater than 9 (e.g., 8 × 2 = 16), then add the digits of the product (e.g., 16 1 + 6 = 7, 18 1 + 8 = 9) or, alternatively, the same result can be found by subtracting 9 from the product (e.g., 16 16 - 9 = 7, 18 18 - 9 = 9).
 Take the sum of all the digits.
 If the total modulo 10 is equal to 0 (if the total ends in zero) then the number is valid according to the Luhn formula; else it is not valid.
 
