752699a140b02fd1a1803c17036a964613f173cf
MattEmond <memond36@gmail.com>
2017-11-21 17:22:58 +0000

    First exercise done

---
+++ week4/w4d2/famous-people/lookup_people.js
@@ -0,0 +1,28 @@
+/*jshint esversion: 6 */
+
+const pg = require("pg");
+const settings = require("./settings");
+
+const arg = process.argv[2];
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query(`SELECT * FROM famous_people where first_name = $1`, [arg], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows[0]);
+    client.end();
+  });
+});
